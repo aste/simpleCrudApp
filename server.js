d1b42bdbe3d34f15aeab1eb2c3c8a771b23500d8
Aste <aste@tuta.io>
2024-05-21 16:51:58 +0200

    Setup the Nodemon and Body-Parser modules

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function () {
   // console.log("May Node be with you");
   app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+  });
+  app.post("/quotes", (req, res) => {
+    // console.log("Hellooooooooooooooooo!");
+    console.log(req.body)
   });
 });
