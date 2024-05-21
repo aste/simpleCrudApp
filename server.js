13f70701b25966927f2433c03919dc6b69654420
Aste <aste@tuta.io>
2024-05-21 17:00:18 +0200

    Add initial setup of MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +12,10 @@ app.listen(3000, function () {
   });
   app.post("/quotes", (req, res) => {
     // console.log("Hellooooooooooooooooo!");
-    console.log(req.body)
+    console.log(req.body);
   });
+});
+
+MongoClient.connect("mongodb-connection-string", (err, client) => {
+  // ... do something here
 });
