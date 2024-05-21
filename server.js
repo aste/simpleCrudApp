47d8edd4b8b2e9af3938830d4c556663692e5115
Aste <aste@tuta.io>
2024-05-21 16:09:58 +0200

    Setup up initial html page load via Express

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.listen(3000, function() {
-    console.log("May Node be with you");
-})
-
+app.listen(3000, function () {
+  // console.log("May Node be with you");
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+});
