8ed577480e6341f96ba20a6d0add9ef2fce6eab4
Aste <aste@tuta.io>
2024-05-25 13:49:54 +0200

    Add DB get functionality

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
 MongoClient.connect(
   "mongodb+srv://aste:4EtTs9h3AOrGljr8@yodacluster.vpzp8qm.mongodb.net/?retryWrites=true&w=majority&appName=yodaCluster"
 )
@@ -17,16 +13,32 @@ MongoClient.connect(
     const db = client.db("yoda-quotes");
     const quoteCollection = db.collection("quotes");
 
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
+
     app.post("/quotes", (req, res) => {
       quoteCollection
         .insertOne(req.body)
         .then((result) => {
-          res.redirect('/');
+          res.redirect("/");
         })
         .catch((error) => {
           console.error(error);
           res.sendStatus(500);
         });
+    });
+
+
+    app.get("/quotes", (req, res) => {
+      quoteCollection
+        .find()
+        .toArray()
+        .then((results) => {
+          console.log(results);
+          res.json(results);
+        })
+        .catch((error) => console.error(error));
     });
 
     app.listen(3000, function () {
