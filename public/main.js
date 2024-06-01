85366061b837ce904eb39db732a64e89e8ed2b7a
Aste <aste@tuta.io>
2024-06-01 15:57:31 +0200

    Add randomly generated quotes for the Post and Put Requests

--- public/main.js
+++ public/main.js
@@ -1,13 +1,28 @@
 const updateButton = document.querySelector("#update-button");
 const deleteButton = document.querySelector("#delete-button");
 
+const vaderQuotes = [
+  "I am your father.",
+  "The Force is strong with this one.",
+  "You underestimate the power of the Dark Side.",
+  "I find your lack of faith disturbing.",
+  "I am altering the deal. Pray I don't alter it any further.",
+  "The ability to destroy a planet is insignificant next to the power of the Force.",
+  "You have failed me for the last time.",
+  "Give yourself to the Dark Side. It is the only way you can save your friends.",
+  "The circle is now complete. When I left you, I was but the learner; now I am the master.",
+  "You don't know the power of the dark side!",
+  "No. I am your father.",
+  "Impressive. Most impressive.",
+];
+
 updateButton.addEventListener("click", (_) => {
   fetch("/quotes", {
     method: "put",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       name: "Darth Vader",
-      quote: "I find your lack of faith disturbing.",
+      quote: `${vaderQuotes[Math.floor(Math.random() * vaderQuotes.length)]}`,
     }),
   })
     .then((res) => {
@@ -33,5 +48,3 @@ deleteButton.addEventListener("click", () => {
       window.location.reload();
     });
 });
-
-
